981f4265af7e69cd3fce47ed6db1e8c5013b5517
Maximka567 <savinovmax250999@gmail.com>
2025-06-25 11:48:48 +0300

    Update and rename products-data.js to js/products-data.js

---
+++ js/products-data.js
@@ -0,0 +1,16 @@
+let products = [
+    {
+        id: 1,
+        name: 'Ãëàç Êòóëõó',
+        description: 'Ôèãóðêà ïåðâîãî áîññà â Terraria',
+        price: 1299,
+        image: 'assets/images/eye-of-cthulhu.jpg' // ëîêàëüíûé ïóòü
+    },
+    {
+        id: 2,
+        name: 'Ñêåëåòðîí',
+        description: 'Ôèãóðêà áîññà Ñêåëåòðîíà',
+        price: 1499,
+        image: 'assets/images/skeletron.jpg' // ëîêàëüíûé ïóòü
+    }
+];

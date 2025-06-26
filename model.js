7c1f686523b2bca9c94ca938f0db4038d80c3a1c
Maximka567 <savinovmax250999@gmail.com>
2025-06-26 09:11:35 +0300

    Add files via upload

---
+++ model.js
@@ -0,0 +1,5 @@
+const material = new THREE.MeshPhongMaterial({
+    color: 0xaaaaaa,      // Основной цвет
+    specular: 0x111111,   // Цвет бликов
+    shininess: 200        // Интенсивность бликов
+});
